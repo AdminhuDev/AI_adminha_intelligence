30656e4dc13de74649af8cbec5f2c6edcc05e2b3
Adminhu <94571098+AdminhuDev@users.noreply.github.com>
2023-07-08 11:59:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import time
 from pyrogram import Client, filters
 from termcolor import colored
 import openai
 from dotenv import load_dotenv
 
-load_dotenv('config.env')
+load_dotenv('.env')
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -13,7 +14,7 @@ api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv('BOT_TOKEN')
 
-app = Client("meu_bot", bot_token=bot_token, api_id=api_id, api_hash=api_hash)
+app = Client("@ai_adminha_intelligence_bot", bot_token=bot_token, api_id=api_id, api_hash=api_hash)
 
 with app:
     me = app.get_me()
@@ -25,13 +26,13 @@ conversas = {}
 def start(client, message):
     message.reply_text(f"Olá {message.from_user.first_name}, seja bem-vindo ao nosso bot! Você pode começar a conversar agora.")
 
-@app.on_message(filters.text & ~filters.command)
+@app.on_message(filters.text & filters.private)
 def reply(client, message):
     # Pega a conversa atual baseado no chat_id
     historico = conversas.get(message.chat.id, "")
 
     # Atualiza o histórico da conversa com a mensagem mais recente
-    historico += f"\nUsuário: {message.text}"
+    historico += f" \nUsuário: {message.text}"
     
     response = openai.Completion.create(
       engine="text-davinci-003",
@@ -43,10 +44,15 @@ def reply(client, message):
 
     # Atualiza o histórico com a resposta da IA
     resposta = response.choices[0].text.strip()
-    historico += f"\nIA: {resposta}"
+    historico += f" \nIA: {resposta}"
 
     # Guarda o histórico atualizado
     conversas[message.chat.id] = historico
+
+    # Imprime a conversa recente
+    print(colored(f"\nUsuário: {message.from_user.id} Mensagem: {message.text}", "red"))
+    print(colored(f"\nIA: {resposta}", "green"))
+    print(colored(f"\n\n{conversas}", "blue"))
 
     if "não entendi" in resposta.lower():
         resposta += "\n\nPor favor, poderia reformular a pergunta?"
